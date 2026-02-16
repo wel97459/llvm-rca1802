Manoj-Kumar-BV/llvm-project

commit e2bab700fd8ddf47d35ad6dad3f8490d2f555dd8

[ELF] Set DF_STATIC_TLS for AArch64/PPC32/PPC64

lld/ELF/Config.h
@@ -436,6 +436,8 @@ struct Ctx {
       backwardReferences;
   // True if SHT_LLVM_SYMPART is used.
   std::atomic<bool> hasSympart{false};
+  // True if there are TLS IE relocations. Set DF_STATIC_TLS if -shared.
+  std::atomic<bool> hasTlsIe{false};
   // True if we need to reserve two .got entries for local-dynamic TLS model.
   std::atomic<bool> needsTlsLd{false};
 

lld/ELF/Arch/Hexagon.cpp
@@ -145,7 +145,6 @@ RelExpr Hexagon::getRelExpr(RelType type, const Symbol &s,
   case R_HEX_IE_GOT_32_6_X:
   case R_HEX_IE_GOT_HI16:
   case R_HEX_IE_GOT_LO16:
-    config->hasTlsIe = true;
     return R_GOTPLT;
   case R_HEX_TPREL_11_X:
   case R_HEX_TPREL_16:

lld/ELF/Arch/RISCV.cpp
@@ -284,7 +284,6 @@ RelExpr RISCV::getRelExpr(const RelType type, const Symbol &s,
   case R_RISCV_TLS_GD_HI20:
     return R_TLSGD_PC;
   case R_RISCV_TLS_GOT_HI20:
-    config->hasTlsIe = true;
     return R_GOT_PC;
   case R_RISCV_TPREL_HI20:
   case R_RISCV_TPREL_LO12_I:

lld/ELF/Arch/X86.cpp
@@ -77,9 +77,6 @@ int X86::getTlsGdRelaxSkip(RelType type) const {
 
 RelExpr X86::getRelExpr(RelType type, const Symbol &s,
                         const uint8_t *loc) const {
-  if (type == R_386_TLS_IE || type == R_386_TLS_GOTIE)
-    config->hasTlsIe = true;
-
   switch (type) {
   case R_386_8:
   case R_386_16:

lld/ELF/Arch/X86_64.cpp
@@ -317,9 +317,6 @@ bool X86_64::deleteFallThruJmpInsn(InputSection &is, InputFile *file,
 
 RelExpr X86_64::getRelExpr(RelType type, const Symbol &s,
                            const uint8_t *loc) const {
-  if (type == R_X86_64_GOTTPOFF)
-    config->hasTlsIe = true;
-
   switch (type) {
   case R_X86_64_8:
   case R_X86_64_16:

lld/ELF/Relocations.cpp
@@ -1283,6 +1283,7 @@ static unsigned handleTlsRelocation(RelType type, Symbol &sym,
 
   if (oneof<R_GOT, R_GOTPLT, R_GOT_PC, R_AARCH64_GOT_PAGE_PC, R_GOT_OFF,
             R_TLSIE_HINT>(expr)) {
+    ctx.hasTlsIe.store(true, std::memory_order_relaxed);
     // Initial-Exec relocs can be relaxed to Local-Exec if the symbol is locally
     // defined.
     if (toExecRelax && isLocalInExecutable) {

lld/ELF/SyntheticSections.cpp
@@ -1348,7 +1348,7 @@ DynamicSection<ELFT>::computeContents() {
   }
   if (!config->zText)
     dtFlags |= DF_TEXTREL;
-  if (config->hasTlsIe && config->shared)
+  if (ctx.hasTlsIe && config->shared)
     dtFlags |= DF_STATIC_TLS;
 
   if (dtFlags)

lld/test/ELF/aarch64-tls-ie.s
@@ -8,15 +8,16 @@
 # RUN: llvm-readobj -d -r %t | FileCheck %s --check-prefix=LE-REL
 # RUN: llvm-objdump -d --no-show-raw-insn %t | FileCheck %s --check-prefix=LE
 
+# IE-REL:      FLAGS STATIC_TLS
 # IE-REL:      .rela.dyn {
-# IE-REL-NEXT:   0x20380 R_AARCH64_TLS_TPREL64 - 0xC
-# IE-REL-NEXT:   0x20378 R_AARCH64_TLS_TPREL64 a 0x0
+# IE-REL-NEXT:   0x20390 R_AARCH64_TLS_TPREL64 - 0xC
+# IE-REL-NEXT:   0x20388 R_AARCH64_TLS_TPREL64 a 0x0
 # IE-REL-NEXT: }
 
 # IE:          adrp    x0, 0x20000
-# IE-NEXT:     ldr     x0, [x0, #0x378]
+# IE-NEXT:     ldr     x0, [x0, #0x388]
 # IE-NEXT:     adrp    x1, 0x20000
-# IE-NEXT:     ldr     x1, [x1, #0x380]
+# IE-NEXT:     ldr     x1, [x1, #0x390]
 
 # LE-REL-NOT:  FLAGS
 # LE-REL:      Relocations [

wel97459/llvm-rca1802

commit 6309043d8d47edfad7bd76c83f2e266ddaac7e2f

[NFC][ELF] Remove unused Config::hasTlsIe (#181680)
This was obsoleted by Ctx::hasTlsIe when the latter was introduced, but
the old Config::hasTlsIe was not removed at the same time.

Fixes: e2bab700fd8d ("[ELF] Set DF_STATIC_TLS for AArch64/PPC32/PPC64")

lld/ELF/Config.h
@@ -486,11 +486,6 @@ struct Config {
   // if that's true.)
   bool isMips64EL;
 
-  // True if we need to set the DF_STATIC_TLS flag to an output file, which
-  // works as a hint to the dynamic loader that the shared object contains code
-  // compiled with the initial-exec TLS model.
-  bool hasTlsIe = false;
-
   // Holds set of ELF header flags for the target.
   uint32_t eflags = 0;
 