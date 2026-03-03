wel97459/llvm-rca1802

commit 25c123b47c2452660b01d18d7f1ef8ea657cffa5

AArch64: Replace @plt/%gotpcrel in data directives with %pltpcrel %gotpcrel (#155776)
Similar to #132569 for RISC-V, replace the unofficial `@plt` and
`@gotpcrel` relocation specifiers, currently only used by clang
-fexperimental-relative-c++-abi-vtables, with %pltpcrel %gotpcrel. The
syntax is not used in humand-written assembly code, and is not supported
by GNU assembler.

Also replace the recent `@funcinit` with `%funcinit(x)`.

llvm/lib/Target/AArch64/MCTargetDesc/AArch64MCAsmInfo.cpp
@@ -114,10 +114,22 @@ StringRef AArch64::getSpecifierName(AArch64::Specifier S) {
   case AArch64::S_GOT_AUTH:            return ":got_auth:";
   case AArch64::S_GOT_AUTH_PAGE:       return ":got_auth:";
   case AArch64::S_GOT_AUTH_LO12:       return ":got_auth_lo12:";
+
+  case AArch64::S_GOTPCREL:            return "%gotpcrel";
+  case AArch64::S_PLT:                 return "%pltpcrel";
+  case AArch64::S_FUNCINIT:            return "%funcinit";
   default:
     llvm_unreachable("Invalid relocation specifier");
   }
   // clang-format on
+}
+
+AArch64::Specifier AArch64::parsePercentSpecifierName(StringRef name) {
+  return StringSwitch<AArch64::Specifier>(name)
+      .Case("pltpcrel", AArch64::S_PLT)
+      .Case("gotpcrel", AArch64::S_GOTPCREL)
+      .Case("funcinit", AArch64::S_FUNCINIT)
+      .Default(0);
 }
 
 static bool evaluate(const MCSpecifierExpr &Expr, MCValue &Res,
@@ -232,8 +244,13 @@ void AArch64MCAsmInfoELF::printSpecifierExpr(
     raw_ostream &OS, const MCSpecifierExpr &Expr) const {
   if (auto *AE = dyn_cast<AArch64AuthMCExpr>(&Expr))
     return AE->print(OS, this);
-  OS << AArch64::getSpecifierName(Expr.getSpecifier());
+  auto Str = AArch64::getSpecifierName(Expr.getSpecifier());
+  OS << Str;
+  if (!Str.empty() && Str[0] == '%')
+    OS << '(';
   printExpr(OS, *Expr.getSubExpr());
+  if (!Str.empty() && Str[0] == '%')
+    OS << ')';
 }
 
 bool AArch64MCAsmInfoELF::evaluateAsRelocatableImpl(

bolt/test/AArch64/skip-non-vfuncptr-reloc-in-relative-vtable.s
@@ -31,5 +31,5 @@ _fake_rtti_data:
 _ZTV3gooE:
 	.word	0
 	.word	_fake_rtti_data-_ZTV3gooE-8
-	.word	foo@PLT-_ZTV3gooE-8
+	.word	%pltpcrel(foo)
 	.size	_ZTV3gooE, 12

lld/test/ELF/aarch64-branch-to-branch.s
@@ -35,14 +35,14 @@ vtable:
 # B2B-NEXT: [[VF:[0-9a-f]{8}]]
 # B2B-RELOC-NEXT: R_AARCH64_PLT32 f3
 # NOB2B-RELOC-NEXT: R_AARCH64_PLT32 f1
-.4byte f1@PLT - vtable
+.4byte %pltpcrel(f1)
 # B2B-SAME: [[VF]]
 # B2B-RELOC-NEXT: R_AARCH64_PLT32 f3+0x4
 # NOB2B-RELOC-NEXT: R_AARCH64_PLT32 f2+0x4
-.4byte f2@PLT - vtable
+.4byte %pltpcrel(f2+4)
 # B2B-SAME: [[VF]]
 # RELOC-NEXT: R_AARCH64_PLT32 f3+0x8
-.4byte f3@PLT - vtable
+.4byte %pltpcrel(f3+8)
 
 .section .text._start,"ax"
 .globl _start

lld/test/ELF/aarch64-feature-bti.s
@@ -290,5 +290,5 @@ func1:
 
 .ifdef RELVTABLE_PLT
 // R_AARCH64_PLT32
-.word funcRelVtable@PLT - .
+.word %pltpcrel(funcRelVtable)
 .endif

lld/test/ELF/aarch64-funcinit64-invalid.s
@@ -5,11 +5,11 @@
 
 .rodata
 # ERR: error: relocation R_AARCH64_FUNCINIT64 cannot be used against local symbol
-.8byte func@FUNCINIT
+.8byte %funcinit(func)
 
 .data
 # ERR: error: relocation R_AARCH64_FUNCINIT64 cannot be used against ifunc symbol 'ifunc'
-.8byte ifunc@FUNCINIT
+.8byte %funcinit(ifunc)
 
 .text
 func:

lld/test/ELF/aarch64-funcinit64.s
@@ -10,7 +10,7 @@
 .data
 # CHECK: R_AARCH64_IRELATIVE [[FOO:[0-9a-f]*]]
 # ERR: relocation R_AARCH64_FUNCINIT64 cannot be used against preemptible symbol 'foo'
-.8byte foo@FUNCINIT
+.8byte %funcinit(foo)
 
 .text
 # CHECK: {{0*}}[[FOO]] {{.*}} foo

lld/test/ELF/aarch64-range-thunk-extension-plt32.s
@@ -25,7 +25,7 @@
   .global _start
   .type _start, %function
 _start:
-  .word callee@PLT - .
+  .word %pltpcrel(callee)
 
   .section .text.2, "ax", %progbits
   .global callee

lld/test/ELF/aarch64-reloc-gotpcrel32.s
@@ -17,11 +17,11 @@ _start:  // PC = 0x303a0
 // bar@GOTPCREL-4 = 0x20390 (got entry for `bar`) - 0x303a8 (.) - 4 = 0xe4fffeff
 // CHECK:      Contents of section .data:
 // CHECK-NEXT:  {{.*}} f0fffeff f0fffeff e4fffeff
-  .word bar@GOTPCREL
-  .word bar@GOTPCREL+4
-  .word bar@GOTPCREL-4
+  .word %gotpcrel(bar)
+  .word %gotpcrel(bar+4)
+  .word %gotpcrel(bar-4)
 
 // WARN: relocation R_AARCH64_GOTPCREL32 out of range: {{.*}} is not in [-2147483648, 2147483647]; references 'baz'
 // WARN: relocation R_AARCH64_GOTPCREL32 out of range: {{.*}} is not in [-2147483648, 2147483647]; references 'baz'
-  .word baz@GOTPCREL+0xffffffff
-  .word baz@GOTPCREL-0xffffffff
+  .word %gotpcrel(baz+0xffffffff)
+  .word %gotpcrel(baz-0xffffffff)

commit bd5d4d0cd326223bff7f8927870dfb3affa2dd19

[AArch64] Fix relative vtable PLT/GOTPCREL specifiers to use MCSpecifierExpr (#184393)
PR #155776 changed S_PLT/S_GOTPCREL printing to
%pltpcrel(x)/%gotpcrel(x)
syntax via MCSpecifierExpr, but lowerSymbolDifference and
getIndirectSymViaGOTPCRel still created MCSymbolRefExpr with these
specifiers, which prints via the @-specifier table as @PLT/@GOTPCREL,
leading to incorrect relocation types.

Fixes: 25c123b47c2452660b01d18d7f1ef8ea657cffa5
(AArch64: Replace @plt/%gotpcrel in data directives with %pltpcrel
%gotpcrel (#155776))

Add llvm/test/CodeGen/AArch64/dso_local_equivalent.ll

llvm/lib/Target/AArch64/AArch64TargetObjectFile.cpp
@@ -25,7 +25,7 @@ using namespace dwarf;
 void AArch64_ELFTargetObjectFile::Initialize(MCContext &Ctx,
                                              const TargetMachine &TM) {
   TargetLoweringObjectFileELF::Initialize(Ctx, TM);
-  PLTRelativeSpecifier = AArch64::S_PLT;
+  PLTPCRelativeSpecifier = AArch64::S_PLT;
   SupportIndirectSymViaGOTPCRel = true;
 
   // AARCH64 ELF ABI does not define static relocation type for TLS offset
@@ -59,11 +59,12 @@ void AArch64_ELFTargetObjectFile::emitPersonalityValueImpl(
 const MCExpr *AArch64_ELFTargetObjectFile::getIndirectSymViaGOTPCRel(
     const GlobalValue *GV, const MCSymbol *Sym, const MCValue &MV,
     int64_t Offset, MachineModuleInfo *MMI, MCStreamer &Streamer) const {
-  int64_t FinalOffset = Offset + MV.getConstant();
-  const MCExpr *Res =
-      MCSymbolRefExpr::create(Sym, AArch64::S_GOTPCREL, getContext());
-  const MCExpr *Off = MCConstantExpr::create(FinalOffset, getContext());
-  return MCBinaryExpr::createAdd(Res, Off, getContext());
+  auto &Ctx = getContext();
+  const MCExpr *Res = MCSymbolRefExpr::create(Sym, Ctx);
+  if (int64_t FinalOffset = Offset + MV.getConstant())
+    Res = MCBinaryExpr::createAdd(Res, MCConstantExpr::create(FinalOffset, Ctx),
+                                  Ctx);
+  return MCSpecifierExpr::create(Res, AArch64::S_GOTPCREL, Ctx);
 }
 
 AArch64_MachoTargetObjectFile::AArch64_MachoTargetObjectFile() {

llvm/lib/Target/AArch64/MCTargetDesc/AArch64MCAsmInfo.cpp
@@ -38,9 +38,6 @@ const MCAsmInfo::AtSpecifier COFFAtSpecifiers[] = {
 
 const MCAsmInfo::AtSpecifier ELFAtSpecifiers[] = {
     {AArch64::S_GOT, "GOT"},
-    {AArch64::S_GOTPCREL, "GOTPCREL"},
-    {AArch64::S_PLT, "PLT"},
-    {AArch64::S_FUNCINIT, "FUNCINIT"},
 };
 
 const MCAsmInfo::AtSpecifier MachOAtSpecifiers[] = {

llvm/test/CodeGen/AArch64/dso_local_equivalent.ll
@@ -1,0 +1,39 @@
+; RUN: llc -mtriple=aarch64 < %s | FileCheck %s --match-full-lines
+
+declare void @extern_func()
+
+; CHECK-LABEL: const:
+; CHECK-NEXT:    .word   %pltpcrel(extern_func)
+
+@const = dso_local constant i32 trunc (i64 sub (i64 ptrtoint (ptr dso_local_equivalent @extern_func to i64), i64 ptrtoint (ptr @const to i64)) to i32)
+
+@_ZTV1B = dso_local constant { [7 x i32] } { [7 x i32] [
+  i32 0,
+  i32 0,
+  i32 trunc (i64 sub (i64 ptrtoint (ptr dso_local_equivalent @f0 to i64), i64 ptrtoint (ptr getelementptr inbounds ({ [7 x i32] }, ptr @_ZTV1B, i32 0, i32 0, i32 2) to i64)) to i32),
+  i32 trunc (i64 sub (i64 ptrtoint (ptr dso_local_equivalent @f1 to i64), i64 ptrtoint (ptr getelementptr inbounds ({ [7 x i32] }, ptr @_ZTV1B, i32 0, i32 0, i32 2) to i64)) to i32),
+  i32 trunc (i64 sub (i64 ptrtoint (ptr dso_local_equivalent @f2 to i64), i64 ptrtoint (ptr getelementptr inbounds ({ [7 x i32] }, ptr @_ZTV1B, i32 0, i32 0, i32 2) to i64)) to i32),
+  i32 trunc (i64 sub (i64 ptrtoint (ptr dso_local_equivalent @f3 to i64), i64 ptrtoint (ptr getelementptr inbounds ({ [7 x i32] }, ptr @_ZTV1B, i32 0, i32 0, i32 2) to i64)) to i32),
+  i32 trunc (i64 sub (i64 ptrtoint (ptr dso_local_equivalent @f4 to i64), i64 ptrtoint (ptr getelementptr inbounds ({ [7 x i32] }, ptr @_ZTV1B, i32 0, i32 0, i32 2) to i64)) to i32)
+] }, align 4
+
+; CHECK-LABEL: _ZTV1B:
+; CHECK-NEXT:    .word   0                               // 0x0
+; CHECK-NEXT:    .word   0                               // 0x0
+; CHECK-NEXT:    .word   %pltpcrel(f0)
+; CHECK-NEXT:    .word   %pltpcrel(f1+4)
+; CHECK-NEXT:    .word   %pltpcrel(f2+8)
+; CHECK-NEXT:    .word   %pltpcrel(f3+12)
+; CHECK-NEXT:    .word   %pltpcrel(f4+16)
+; CHECK-NEXT:    .size   _ZTV1B, 28
+declare void @f0()
+declare void @f1()
+define dso_local void @f2() {
+  ret void
+}
+define void @f3() {
+  ret void
+}
+define hidden void @f4() {
+  ret void
+}

llvm/test/MC/ELF/rtti-proxy-gotpcrel.ll
@@ -1,7 +1,7 @@
 ; REQUIRES: x86-registered-target && aarch64-registered-target && riscv-registered-target
 ; RUN: llc %s -mtriple=x86_64 -o - | FileCheck %s
-; RUN: llc %s -mtriple=aarch64 -o - | FileCheck %s
-; RUN: llc %s -mtriple=riscv64 -o - | FileCheck %s --check-prefix=RISCV
+; RUN: llc %s -mtriple=aarch64 -o - | FileCheck %s --check-prefix=SPECIFIER
+; RUN: llc %s -mtriple=riscv64 -o - | FileCheck %s --check-prefix=SPECIFIER
 
 @vtable = dso_local unnamed_addr constant i32 trunc (i64 sub (i64 ptrtoint (ptr @rtti.proxy to i64), i64 ptrtoint (ptr @vtable to i64)) to i32), align 4
 @vtable_with_offset = dso_local unnamed_addr constant [2 x i32] [i32 0, i32 trunc (i64 sub (i64 ptrtoint (ptr @rtti.proxy to i64), i64 ptrtoint (ptr @vtable_with_offset to i64)) to i32)], align 4
@@ -29,11 +29,11 @@
 ; CHECK-NEXT:    .{{word|long}}   rtti@GOTPCREL-4{{$}}
 ; CHECK-NEXT:    .{{word|long}}   0
 
-; RISCV-LABEL: vtable:
-; RISCV-NEXT:    .word   %gotpcrel(rtti+0)
-; RISCV-LABEL: vtable_with_offset:
-; RISCV-NEXT:    .word   0
-; RISCV-NEXT:    .word   %gotpcrel(rtti+4)
-; RISCV-LABEL: vtable_with_negative_offset:
-; RISCV-NEXT:    .word   %gotpcrel(rtti-4)
-; RISCV-NEXT:    .word   0
+; SPECIFIER-LABEL: vtable:
+; SPECIFIER-NEXT:    .word   %gotpcrel(rtti{{(\+0)?}})
+; SPECIFIER-LABEL: vtable_with_offset:
+; SPECIFIER-NEXT:    .word   0
+; SPECIFIER-NEXT:    .word   %gotpcrel(rtti+4)
+; SPECIFIER-LABEL: vtable_with_negative_offset:
+; SPECIFIER-NEXT:    .word   %gotpcrel(rtti-4)
+; SPECIFIER-NEXT:    .word   0